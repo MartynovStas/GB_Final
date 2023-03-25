41b9813fc811608f5b61cd454446adcbed691d3c
Stas Martynov <maks.mart71@bk.ru>
2023-03-25 20:29:09 +0400

    создал програму

---
+++ Pogram/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"324582", "mat", "mortol", "NBA", "clocs"};
+string[] array2 = new string[array1.Length];
+void ArrayWitchiw(string[] array1, string[] array2)
+{
+    int caunt = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[caunt] = array1[i];
+        caunt++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayWitchiw(array1, array2);
+PrintArray(array2);
